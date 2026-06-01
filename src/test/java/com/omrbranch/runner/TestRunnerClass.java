f40fde12e4aba9ccca264c6000b17a0e18f6805e
Prasanth <prasanthparthiban777@gmail.com>
2026-06-01 16:19:45 +0530

    tester2 commit

--- src/test/java/com/omrbranch/runner/TestRunnerClass.java
+++ src/test/java/com/omrbranch/runner/TestRunnerClass.java
@@ -1,5 +1,43 @@
 package com.omrbranch.runner;
 
-public class TestRunnerClass {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
+public class TestRunnerClass {
+	private void T0101_omr2() {
+    	//Open omrbranch
+    	WebDriver driver = new ChromeDriver();
+    	driver.manage().window().maximize();
+		driver.get("https://www.omrbranch.com");
+		String title1 = driver.getTitle();
+		System.out.println(title1);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);		
+	}	
+    private void T0102_traniningInOmr2() {
+    	//Open traininginomr
+    	WebDriver driver = new ChromeDriver();
+    	driver.manage().window().maximize();
+		driver.get("https://www.traininginomr.co.in");
+		String title2 = driver.getTitle();
+		System.out.println(title2);
+		String currentUrl1 = driver.getCurrentUrl();
+		System.out.println(currentUrl1);				
+	}
+    private void T0103_greensTech2() {
+    	//Open greenstech
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://greenstech.in.");
+		String title3 = driver.getTitle();
+		System.out.println(title3);
+		String currentUrl2 = driver.getCurrentUrl();
+		System.out.println(currentUrl2);
+}
+    public static void main(String[] args) {
+		TestRunnerClass task1 = new TestRunnerClass();
+		task1.T0101_omr2();
+		task1.T0102_traniningInOmr2();
+		task1.T0103_greensTech2();
+}
 }
